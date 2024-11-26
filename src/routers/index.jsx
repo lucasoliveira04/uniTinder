aff027b0e52762c8d83efcf31868388d58f881d0
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-11-26 10:38:55 -0300

    feat : script de rotas

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { HOME } from "../constant"
+import { HOME, REGISTER } from "../constant"
 import { HomePage } from "../pages/home"
+import { RegisterPage } from "../pages/register"
 
 export const Routers = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={HOME} element={<HomePage/>}/>
+                <Route path={REGISTER} element={<RegisterPage/>}/>
 
                 <Route path="*" element={<HomePage/>}/>
             </Routes>
